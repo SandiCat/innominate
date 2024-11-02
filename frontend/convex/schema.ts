f3490866a69baa0c22ee02246f37b8b182459a78
Sandi <sandi.dusic@protonmail.com>
2024-11-02 10:32:27 +0100

    before getting rid of tree stuff

---
+++ frontend/convex/schema.ts
@@ -0,0 +1,26 @@
+import { defineSchema, defineTable } from "convex/server";
+import { authTables } from "@convex-dev/auth/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  user: defineTable({}), // just the ID
+  notes: defineTable({
+    content: v.string(),
+    parentId: v.optional(v.id("notes")),
+    userId: v.id("users"),
+  }).index("by_user", ["userId"]),
+  canvases: defineTable({
+    name: v.string(),
+    userId: v.id("users"),
+  }).index("by_user", ["userId"]),
+  canvasItems: defineTable({
+    canvasId: v.id("canvases"),
+    noteId: v.id("notes"),
+    position: v.object({
+      x: v.number(),
+      y: v.number(),
+    }),
+  })
+    .index("by_canvas", ["canvasId"])
+    .index("by_note", ["noteId"]),
+});
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexReactClient } from "convex/react";
+
 import "./index.css";
 import App from "./App.tsx";
 // import CoordsTest from "./junk/CoordsTest.tsx";
 // import NoteTreeShowCase from "./junk/NoteTreeShowCase";
 
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ConvexAuthProvider client={convex}>
+      <div> hey how ya doing</div>
+    </ConvexAuthProvider>
   </StrictMode>
 );
