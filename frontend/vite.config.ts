7d7e0ce673b2110decf18a797a0399e6f73588b3
Sandi <sandi.dusic@protonmail.com>
2024-11-20 17:35:54 +0100

    try to kill service worker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,10 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({ registerType: "autoUpdate", selfDestroying: true }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
